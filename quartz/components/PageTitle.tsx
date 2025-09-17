e8a67016fe6eadf983cd282e89c712202657ef58
Adam <adam@canny.io>
2025-09-17 16:13:31 -0600

    Show text for title on mobile

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,35 +1,19 @@
 import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import styles from "./styles/pageTitle.scss"
 
 const PageTitle: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <div class="page-title">
-      <a href={baseDir}><img src="/static/logo.jpg" /></a>
+      <a href={baseDir}>
+        <img src="/static/logo.jpg" />
+        <span>Hi, I'm Adam</span>
+      </a>
     </div>
   )
 }
 
-PageTitle.css = `
-.page-title {
-  display: flex;
-  max-width: 14rem;
-}
-.page-title a {
-  border-radius: 50%;
-  border: 4px solid var(--dark);
-  width: 8rem;
-  height: 8rem;
-  margin: auto;
-  overflow: hidden;
-}
-.page-title a:hover {
-  filter: none;
-}
-
-.page-title img {
-  margin: 0;
-}
-`
+PageTitle.css = styles;
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
